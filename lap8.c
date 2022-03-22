0adcee68a123e96a072e7bab3d99a7d52cbdd145
HienHy <hiennguyenhy0911@gmail.com>
2022-03-22 19:50:26 +0700

    thêm bài 8

--- lap8.c
+++ lap8.c
@@ -1,11 +1,24 @@
 #include<stdio.h>
-
 int main(){
-	int n,i,max,min,a;
-	printf("nhap n = ");
+	int n,i,num,max,min;
+	printf("nhap vao so n = ");
 	scanf("%d",&n);
-	for(i=1;i<=n;i++){
-		printf("so can so sanh ");
-		scanf("%d",&a);}
+	for(i=0;i<=n;i++){
+		printf("nhap nhung so can xac dinh :");
+		scanf("%d",&num);
+		if(i==0){
+			max=num;
+			min=num;
+			}
+			if(i>0 && num>max){
+				max=num;}
+		    if(i>0 && num<min){
+		    	min=num;}
+		    	}
+		    	printf("so lon nhat %d\n",max);
+		    	printf("so nho nhat %d",min);
 		
-		}
+		
+	}
+		
+	
